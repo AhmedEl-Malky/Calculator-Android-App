36c83fc3687e0d5f451d54ef96ab087f8e70b6f9
Ahmed Ramadan El_Malky <130024306+AhmedEl-Malky@users.noreply.github.com>
2024-01-30 20:02:00 +0200

    optimize the code and final touches on UI (#5)
    * delete some redundancy code
    
    * add app icon
    
    * optimize the code and final touches on UI

--- app/src/main/java/com/elmalky/calculator/UI/MainActivity.kt
+++ app/src/main/java/com/elmalky/calculator/UI/MainActivity.kt
@@ -2,7 +2,6 @@ package com.elmalky.calculator.UI
 
 import android.R
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -16,67 +15,74 @@ import com.elmalky.calculator.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     var ans = ""
     lateinit var binder: ActivityMainBinding
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("Destroy", "destroy")
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i("create", "cre")
         binder = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binder.root)
-        AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
         binder.switchTheme.isChecked =
             AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
-        binder.switchTheme.setOnCheckedChangeListener { buttonView, isChecked ->
-            changeTheme(isChecked)
-        }
-        binder.equalBtn.setOnClickListener {
-            var expression: String? = binder.inputExpression.text.toString()
-            var postfixExpression: String? = infixToPostfix(expression)
-            var result = String.format("%.3f", evaluateExpression(postfixExpression))
-            if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
-                binder.outputResult.setTextColor(ContextCompat.getColor(this, R.color.white))
-            else
-                binder.outputResult.setTextColor(ContextCompat.getColor(this, R.color.black))
-            while (result.last() == '0')
-                result = result.dropLast(1)
-            if (result.last() == '.')
-                result = result.dropLast(1)
-            binder.outputResult.text = result
-            ans = result
-
-        }
-        binder.ansBtn.setOnClickListener {
-            binder.inputExpression.text = ans
-        }
         binder.apply {
+            switchTheme.setOnCheckedChangeListener { buttonView, isChecked ->
+                changeTheme(isChecked)
+            }
+            equalBtn.setOnClickListener {
+                equalBtnClick()
+            }
             CBtn.setOnClickListener {
-                inputExpression.text = ""
-                outputResult.text = "0"
+                CBtnClick()
             }
             delBtn.setOnClickListener {
-                inputExpression.text = inputExpression.text.dropLast(1)
-                if (binder.inputExpression.text.isEmpty())
-                    binder.outputResult.text = "0"
-                else {
-                    try {
-                        var expression: String? = binder.inputExpression.text.toString()
-                        var postfixExpression: String? = infixToPostfix(expression)
-                        var result = String.format("%.3f", evaluateExpression(postfixExpression))
-
-                        while (result.last() == '0')
-                            result = result.dropLast(1)
-                        if (result.last() == '.')
-                            result = result.dropLast(1)
-                        binder.outputResult.text = result
-                    } catch (e: Exception) {
-                    }
-                }
-
+                delBtnClick()
+            }
+            ansBtn.setOnClickListener {
+                ansBtnClick()
             }
         }
+    }
+
+    private fun delBtnClick() {
+        binder.inputExpression.text = binder.inputExpression.text.dropLast(1)
+        if (binder.inputExpression.text.isEmpty())
+            binder.outputResult.text = "0"
+        else {
+            try {
+                var expression: String? = binder.inputExpression.text.toString()
+                var postfixExpression: String? = infixToPostfix(expression)
+                var result = String.format("%.3f", evaluateExpression(postfixExpression))
+
+                while (result.last() == '0')
+                    result = result.dropLast(1)
+                if (result.last() == '.')
+                    result = result.dropLast(1)
+                binder.outputResult.text = result
+            } catch (e: Exception) {
+            }
+        }
+    }
+
+    private fun CBtnClick() {
+        binder.inputExpression.text = ""
+        binder.outputResult.text = "0"
+    }
+
+    private fun equalBtnClick() {
+        var expression: String? = binder.inputExpression.text.toString()
+        var postfixExpression: String? = infixToPostfix(expression)
+        var result = String.format("%.3f", evaluateExpression(postfixExpression))
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
+            binder.outputResult.setTextColor(ContextCompat.getColor(this, R.color.white))
+        else
+            binder.outputResult.setTextColor(ContextCompat.getColor(this, R.color.black))
+        while (result.last() == '0')
+            result = result.dropLast(1)
+        if (result.last() == '.')
+            result = result.dropLast(1)
+        binder.outputResult.text = result
+        ans = result
+    }
+
+    private fun ansBtnClick() {
+        binder.inputExpression.text = ans
     }
 
     private fun changeTheme(checked: Boolean) {
